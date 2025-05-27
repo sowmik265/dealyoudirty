fa219b18da828cef3854666b4b027b2d40336d8e
Tanvir Ahamed <sowmik265@gmail.com>
2025-05-28 00:23:37 +0600

    fixed again

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/header";
+import Header from "../components/Header";
 import Footer from "@/components/Footer";
 import Banner from "@/components/Banner";
 import MarqueeBanner from "@/components/MarqueeBanner";
